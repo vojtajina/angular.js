d362432ab9cd67f5c674c5dd6ba690c8e663aa76
Vojta Jina <vojta.jina@gmail.com>
2012-03-26 13:10:18 -0700

    refactor($sniffer): remove docs, not use Modernizr

--- src/service/sniffer.js
+++ src/service/sniffer.js
@@ -1,20 +1,11 @@
 'use strict';
 
 /**
- * @ngdoc object
- * @name angular.module.ng.$sniffer
- * @requires $window
- *
- * @property {boolean} history Does the browser support html5 history api ?
- * @property {boolean} hashchange Does the browser support hashchange event ?
- *
- * @description
  * This is very simple implementation of testing browser's features.
+ * It's used only internally by $browser, $location
  */
 function $SnifferProvider(){
   this.$get = ['$window', function($window){
-    if ($window.Modernizr) return $window.Modernizr;
-
     return {
       history: !!($window.history && $window.history.pushState),
       hashchange: 'onhashchange' in $window &&
