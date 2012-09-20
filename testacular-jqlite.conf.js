0e83e6cec1720e734c616f32c0cb7b738ef52c25
Vojta Jina <vojta.jina@gmail.com>
2012-09-19 19:40:54 -0700

    WIP:

--- testacular-jqlite.conf.js
+++ testacular-jqlite.conf.js
@@ -6,4 +6,9 @@ exclude = ['**/*jasmine*/**', '**/*jstd*/**'].concat(angularFiles.files.jstdExcl
 autoWatch = true;
 logLevel = LOG_INFO;
 logColors = true;
-browsers = ['Chrome']
+browsers = ['Chrome'];
+
+junitReporter = {
+  outputFile: 'junit/jqlite.xml',
+  suite: 'jqlite'
+};
