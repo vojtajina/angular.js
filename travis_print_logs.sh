15dd20bbb5e831d264dd61d103811ce5e90cd514
Vojta Jina <vojta.jina@gmail.com>
2013-12-10 16:04:22 -0800

    WIP: travis nested folding take 2

--- travis_print_logs.sh
+++ travis_print_logs.sh
@@ -4,10 +4,10 @@ LOG_FILES=$LOGS_DIR/*
 
 for FILE in $LOG_FILES; do
   echo -e "\n\n\n"
-  echo -en "travis_fold:start:$FILE\r"
+  echo -e "travis_fold:start:$FILE"
   echo "================================================================================"
   echo " $FILE"
   echo "================================================================================"
   cat $FILE
-  echo -en "travis_fold:end:$FILE\r"
+  echo -e "travis_fold:end:$FILE"
 done
