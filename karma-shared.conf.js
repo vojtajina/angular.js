c9382c0ac65268cdc69f3a84b953d63752408eb6
Vojta Jina <vojta.jina@gmail.com>
2014-05-23 14:48:17 +0200

    WIP: run unit tests on BS

--- karma-shared.conf.js
+++ karma-shared.conf.js
@@ -7,7 +7,7 @@ module.exports = function(config, specificOptions) {
     browsers: ['Chrome'],
     browserDisconnectTimeout: 10000,
     browserDisconnectTolerance: 2,
-    browserNoActivityTimeout: 20000,
+    browserNoActivityTimeout: 60000,
 
 
     // SauceLabs config for local development.
